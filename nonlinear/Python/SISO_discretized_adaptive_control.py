0b8c35f7c0eff4f6cd24f0854147975d4e7394d2
Joshua Ramayrat <joshua@ramayrat.com>
2024-01-09 15:07:24 -0800

    Update adaptive sim. This doesn't work yet

--- nonlinear/Python/SISO_discretized_adaptive_control.py
+++ nonlinear/Python/SISO_discretized_adaptive_control.py
@@ -25,6 +25,7 @@ from matplotlib.pyplot import *
 from typing import List, Any
 
 import math
+import matplotlib.pyplot as plt
 
 from numpy import linspace
 import numpy as np
@@ -74,10 +75,15 @@ def main():
   gain_D_coeff = 5
 
   # Array declarations to store errors, outputs, and inputs for the simulation.
-  error_system      = []
+  e_arr             = []
+  e_dot_arr         = []
   error_k_param     = []
   error_d_param     = []
   controller_inputs = []
+  k_est_values      = []
+  d_est_values      = []
+  x_d_arr           = []
+  x_arr             = []
 
   e_curr = 0
   e_prev = 0
@@ -85,37 +91,60 @@ def main():
   x_ref_curr = 0
   x_sim_curr = 0
 
+  integral_k = 0
+  integral_d = 0
+
+  print("Hello")
   ################################################
   #### Simulation for adaptive controller ########
   ################################################
-  for i in range(1, len(t)-1):
+  for i in range(1, len(t)-2):
+    print("At index ", i)
     # Updating time variables
     t_curr = t[i]
     dt = t_curr - t_prev
     t_prev = t_curr
 
-    # Reference model output
+    # Reference model output. I think this is x_d(t) for the assignment
     x_ref[i+2] = 2*x_ref[i+1] - x_ref[i] + (- k_actual*x_ref[i] + ref[i] + d_actual*np.sin(10*math.pi*i))*dt**2 
+    x_d_arr.append(x_ref[i+2])
 
     curr_k_param_error = k_actual - k_est
     curr_d_param_error = d_actual - d_est
+
+    # Appending estimated parameter values to arrays for plotting
+    error_k_param.append(curr_k_param_error)
+    error_d_param.append(curr_d_param_error)
+    k_est_values.append(k_est)
+    d_est_values.append(d_est)
 
     x_ref_curr = x_ref[i+2]
 
     e_curr = x_ref_curr - x_sim_curr
 
     ## Computing for parameters and values that go into the controller equation.
-    x_ddot_desired = x_ref[i+2] - 2*x_ref[i+1] + x_ref[i] / dt**2
+    x_ddot_desired = (x_ref[i+2] - 2*x_ref[i+1] + x_ref[i]) / dt**2
     e_dot = (e_curr - e_prev) / dt
     s = e_dot + gain_lambda*e_curr
 
+    # Debug
+    print("Output of s: ", s)
+
+    # Appending system error values to arrays for plotting
+    e_arr.append(e_curr)
+    e_dot_arr.append(e_dot)
+
     # Parameter adaptation integrals
-    integral_k += s*x_sim*dt
+    integral_k += s*x_sim_curr*dt
     integral_d += -np.sin(10*math.pi*i)*s*dt
 
     # Adaptation laws
     k_est = k_est_init + gain_K_coeff*integral_k
     d_est = d_est_init + gain_D_coeff*integral_d
+
+    # For debugging
+    print("Output of adaptation law for k_est: ", k_est)
+    print("Output of adaptation law for d_est: ", d_est)
 
     # Equation for adaptive controller
     controller_input = x_ddot_desired + k_est*x_ref[i+2] - d_est*np.sin(10*math.pi*i) + gain_lambda*e_dot + gain_eta*s
@@ -123,5 +152,20 @@ def main():
     # Output of the simulated system, provided with controller and estimated parameters computed through adaptation laws.
     x_sim[i+2] = 2*x_sim[i+1] - x_sim[i] + (- k_est*x_sim[i] + controller_input + d_est*np.sin(10*math.pi*i))*dt**2
     x_sim_curr = x_sim[i+2]
+    x_arr.append(x_sim_curr)
+
 
     e_prev = e_curr
+
+  fig, axes = plt.subplots(1)
+
+  axes.plot(t[:999], x_arr, label='output')
+  axes.set_title('Actual output')
+  axes.set_ylabel('Amplitude')
+  axes.set_xlabel('Time (seconds)')
+
+  plt.legend()
+  plt.show()
+
+if __name__ == "__main__":
+  main()
